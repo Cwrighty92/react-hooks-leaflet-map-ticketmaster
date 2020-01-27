43df26f20a060e1481a57e0434a2e173eeb36abd
Chris Wright <chriswrighty1192@gmail.com>
2020-01-27 22:40:15 +0000

    added leaflet using hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import L from "leaflet";
 
-const App: React.FC = () => {
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Map />
     </div>
   );
+};
+
+function Map() {
+  useEffect(() => {
+    // create map
+    L.map("map", {
+      center: [49.8419, 24.0315],
+      zoom: 16,
+      layers: [
+        L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
+          attribution:
+            '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        })
+      ]
+    });
+  }, []);
+
+  return <div id="map"></div>;
 }
 
 export default App;
